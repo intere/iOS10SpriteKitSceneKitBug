9661c2b78236e6492b080c4cfd4d2178f80d251c
Eric Internicola <intere@gmail.com>
2016-09-04 11:40:26 -0600

    Updated the GameViewController with comments about what is being done, and the bug description.

--- ProofOfConcept/GameViewController.swift
+++ ProofOfConcept/GameViewController.swift
@@ -11,45 +11,48 @@ import QuartzCore
 import SpriteKit
 import SceneKit
 
+/**
+ * This class demonstrates a Bug in iOS10 when embedding a SceneKit Scene within a SpriteKitScene via a SK3DScene object
+ */
 class GameViewController: UIViewController {
 
     var skView: SKView? {
         return self.view as? SKView
     }
 
-    var scnNode: SK3DNode?
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 1.  Ensure we can get a reference to the SKView
         guard let skView = skView else {
             assert(false, "We don't have an SKView")
             return
         }
         
-        // create a new SCNScene
+        // 2.  Load the SCNScene (includes ship, camera and light)
         let scene = SCNScene(named: "art.scnassets/scene.scn")!
 
-        // retrieve the ship node
+        // 3.  Get a reference to the ship
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
         
-        // animate the 3d object
+        // 4.  Begin animating the Ship
         ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
 
+        // 5.  Create an SK3DNode to load the SCNScene into:
         let scnNode = SK3DNode(viewportSize: skView.bounds.size)
         scnNode.scnScene = scene
-        
-        // configure the view
-        skView.backgroundColor = UIColor.green
 
-        // Create the SKScene
+        // 6.  Create an SKScene
         let skScene = SKScene(size: view.frame.size)
-        skScene.scaleMode = .resizeFill
-        skScene.addChild(scnNode)
-        scnNode.position = CGPoint(x: skScene.frame.midX, y: skScene.frame.midY)
 
+        // 7.  Position and add the SK3DNode to the SKScene:
+        scnNode.position = CGPoint(x: skScene.frame.midX, y: skScene.frame.midY)
+        skScene.addChild(scnNode)
+
+        // 8.  Present the SKScene
         skView.presentScene(skScene)
-        self.scnNode = scnNode
+
+        // BUG: Note that the ship does not animate, unless you tap the view, then you get an updated redraw of the scene
     }
     
     override var shouldAutorotate: Bool {
