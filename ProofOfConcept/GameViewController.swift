bea820d2eb58418bdca1efe3b2a185900e125c14
Eric Internicola <intere@gmail.com>
2016-09-04 11:34:36 -0600

    Initial Commit, this project demonstrates Creating an SK3DNode inside of a SKScene and how the SK3DNode doesn't refresh on update.

--- ProofOfConcept/GameViewController.swift
+++ ProofOfConcept/GameViewController.swift
@@ -8,97 +8,48 @@
 
 import UIKit
 import QuartzCore
+import SpriteKit
 import SceneKit
 
 class GameViewController: UIViewController {
 
+    var skView: SKView? {
+        return self.view as? SKView
+    }
+
+    var scnNode: SK3DNode?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        guard let skView = skView else {
+            assert(false, "We don't have an SKView")
+            return
+        }
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
+        // create a new SCNScene
+        let scene = SCNScene(named: "art.scnassets/scene.scn")!
+
         // retrieve the ship node
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
         
         // animate the 3d object
         ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+
+        let scnNode = SK3DNode(viewportSize: skView.bounds.size)
+        scnNode.scnScene = scene
         
         // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
-    }
-    
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
+        skView.backgroundColor = UIColor.green
+
+        // Create the SKScene
+        let skScene = SKScene(size: view.frame.size)
+        skScene.scaleMode = .resizeFill
+        skScene.addChild(scnNode)
+        scnNode.position = CGPoint(x: skScene.frame.midX, y: skScene.frame.midY)
+
+        skView.presentScene(skScene)
+        self.scnNode = scnNode
     }
     
     override var shouldAutorotate: Bool {
